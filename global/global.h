e0cf5441bb8488b916db02669174cf5b5ea7dfee
Mariamehab2003 <99998896+Mariamehab2003@users.noreply.github.com>
2023-07-23 01:01:20 +0300

    Add files via upload

---
+++ global/global.h
@@ -0,0 +1,15 @@
+
+
+
+#define GLOBAL H
+typedef struct elementtype
+{
+    int entry;
+} ElementType;
+#define MAXSIZE 100
+typedef ElementType StackEntry;
+typedef ElementType QueueEntry;
+#define maxqueue MAXSIZE
+#define maxstack MAXSIZE
+
+
